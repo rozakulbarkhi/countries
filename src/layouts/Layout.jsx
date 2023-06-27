df143fe0d63200f72dcd92569c56ab5012a5854d
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-06-27 19:13:12 +0700

    fix

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-hidden">
       <Navbar />
       {children}
     </div>
