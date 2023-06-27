2cf48fd4c4a1a56e51ff76ecbdd076f8eb979257
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-06-27 18:54:46 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../src/pages/Home.jsx";
 import Detail from "../src/pages/Detail.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    // dynamic route
+    {
+      path: "/:country",
+      element: <Detail />,
+    },
+  ],
   {
-    path: "/",
-    element: <Home />,
-  },
-  // dynamic route
-  {
-    path: "/:country",
-    element: <Detail />,
-  },
-]);
+    basename: "/countries",
+  }
+);
 
 const App = () => {
   return router;
